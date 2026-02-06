7484f40f264256539fafd4f9044972f09b983b89
Jeff Boek <jboek@mozilla.com>
2026-02-06 12:40:38 -0800

    Bug 2015109 - Integrates lib-integrity-googleplay in Fenix

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
@@ -18,6 +18,10 @@ import mozilla.components.feature.addons.update.DefaultAddonUpdater
 import mozilla.components.feature.autofill.AutofillConfiguration
 import mozilla.components.lib.crash.store.CrashAction
 import mozilla.components.lib.crash.store.CrashMiddleware
+import mozilla.components.lib.integrity.googleplay.GooglePlayIntegrityClient
+import mozilla.components.lib.integrity.googleplay.GoogleProjectNumber
+import mozilla.components.lib.integrity.googleplay.RequestHashProvider
+import mozilla.components.lib.integrity.googleplay.TokenProviderFactory
 import mozilla.components.lib.publicsuffixlist.PublicSuffixList
 import mozilla.components.support.base.android.DefaultProcessInfoProvider
 import mozilla.components.support.base.android.NotificationsDelegate
@@ -354,6 +358,16 @@ class Components(private val context: Context) {
             distributionProviderChecker = DefaultDistributionProviderChecker(context),
             distributionSettings = DefaultDistributionSettings(settings),
             metricController = analytics.metrics,
+        )
+    }
+
+    val integrityClient by lazyMonitored {
+        GooglePlayIntegrityClient(
+            TokenProviderFactory.create(
+                context,
+                GoogleProjectNumber.create(BuildConfig.GPS_INTEGRITY_TOKEN),
+            ),
+            RequestHashProvider.randomHashProvider(),
         )
     }
 
