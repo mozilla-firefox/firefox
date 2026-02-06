091f959871a612106d5d25bf41f6c73e0ca2a432
Jeff Boek <jboek@mozilla.com>
2026-02-06 10:38:26 -0800

    Bug 2015109 - Implements GooglePlayIntegrityClient

--- mobile/android/android-components/components/lib/integrity-googleplay/src/main/java/mozilla/components/lib/integrity/googleplay/GooglePlayIntegrityClient.kt
+++ mobile/android/android-components/components/lib/integrity-googleplay/src/main/java/mozilla/components/lib/integrity/googleplay/GooglePlayIntegrityClient.kt
@@ -4,9 +4,67 @@
 
 package mozilla.components.lib.integrity.googleplay
 
+import android.content.Context
+import com.google.android.play.core.integrity.IntegrityManagerFactory
 import mozilla.components.concept.integrity.IntegrityClient
 import mozilla.components.concept.integrity.IntegrityToken
+import mozilla.components.lib.integrity.googleplay.ext.prepare
+import java.util.UUID
+import kotlin.uuid.Uuid
 
-class GooglePlayIntegrityClient: IntegrityClient {
-    override suspend fun request(): Result<IntegrityToken> = Result.success(IntegrityToken("TOKEN"))
+@JvmInline
+value class CloudProjectNumber(val value: Long) {
+    companion object {
+        val test = CloudProjectNumber(31337L)
+    }
 }
+
+fun interface TokenProvider {
+    suspend fun request(requestHashProvider: RequestHashProvider): Result<IntegrityToken>
+}
+
+fun interface TokenProviderFactory {
+    suspend fun create(): Result<TokenProvider>
+
+    companion object {
+        fun create(context: Context, cloudProjectNumber: CloudProjectNumber): TokenProviderFactory {
+            return GooglePlayTokenProviderFactory(context, cloudProjectNumber)
+        }
+    }
+}
+
+fun interface RequestHashProvider {
+    fun generateHash(): String
+
+    companion object {
+        fun randomHashProvider() = RequestHashProvider {
+            UUID.randomUUID().toString()
+        }
+    }
+}
+
+private class GooglePlayTokenProviderFactory(
+    context: Context,
+    private val cloudProjectNumber: CloudProjectNumber,
+) : TokenProviderFactory {
+    private val integrityManager = IntegrityManagerFactory.createStandard(context)
+    override suspend fun create(): Result<TokenProvider> = integrityManager.prepare(cloudProjectNumber)
+}
+
+class GooglePlayIntegrityClient(
+    private val tokenProviderFactory: TokenProviderFactory,
+    private val requestHashProvider: RequestHashProvider,
+) : IntegrityClient {
+    var tokenProvider: TokenProvider? = null
+
+    suspend fun warmup() {
+        if (tokenProvider == null) {
+            tokenProvider = tokenProviderFactory.create().getOrNull()
+        }
+    }
+
+    override suspend fun request() = tokenProvider?.request(requestHashProvider)
+        ?: Result.failure(MissingTokenProvider())
+}
+
+class MissingTokenProvider : IllegalStateException("GooglePlayIntegrityClient is missing a token provider")
