fcd27db89473ec29b2c54c99151395742fc324da
Jeff Boek <jboek@mozilla.com>
2026-02-06 09:29:42 -0800

    Bug 2015109 - Scaffolds gradle modules for integrity APIs

---
+++ mobile/android/android-components/components/concept/integrity/src/main/java/mozilla/components/concept/integrity/IntegrityClient.kt
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.concept.integrity
+
+@JvmInline
+value class IntegrityToken(val value: String)
+
+/**
+ * Interface used to fetch an integrity token
+ */
+fun interface IntegrityClient {
+    /**
+     * Requests an [IntegrityToken]
+     */
+    suspend fun request(): Result<IntegrityToken>
+}
