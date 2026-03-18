18110485945cbd7c076311e278cd7c4ee61f7486
fmasalha <fmasalha@mozilla.com>
2026-03-18 08:06:48 -0400

    Bug 2024167 - Added haptic feedback on summary in s2s

--- mobile/android/android-components/components/feature/summarize/src/main/java/mozilla/components/feature/summarize/SummarizationScreen.kt
+++ mobile/android/android-components/components/feature/summarize/src/main/java/mozilla/components/feature/summarize/SummarizationScreen.kt
@@ -26,7 +26,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -86,7 +88,15 @@ private fun SummarizationScreen(
     store: SummarizationStore,
     settingsStore: SummarizeSettingsStore? = null,
 ) {
+    val haptic = LocalHapticFeedback.current
+
     val state by store.stateFlow.collectAsStateWithLifecycle()
+
+    LaunchedEffect(state) {
+        if (state is SummarizationState.Summarized) {
+            haptic.performHapticFeedback(HapticFeedbackType.Confirm)
+        }
+    }
 
     SummarizationScreenScaffold(
         modifier = modifier
