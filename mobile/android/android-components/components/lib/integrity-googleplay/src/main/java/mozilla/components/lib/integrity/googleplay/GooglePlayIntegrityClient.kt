733d992a8ea0cebc55004d7b11007982012121e7
Jeff Boek <j@jboek.com>
2026-04-01 18:50:04 -0700

    Bug 2028702 - Documents lib-integrity-googleplay
    This patch also does a slight refactor of `request()` to be more
    idiomatic Kotlin versus the double `fold()` on `Result` to make
    it easier to document.

--- mobile/android/android-components/components/lib/integrity-googleplay/src/main/java/mozilla/components/lib/integrity/googleplay/GooglePlayIntegrityClient.kt
+++ mobile/android/android-components/components/lib/integrity-googleplay/src/main/java/mozilla/components/lib/integrity/googleplay/GooglePlayIntegrityClient.kt
@@ -222,25 +222,19 @@ class GooglePlayIntegrityClient(
      * @return A [Result] containing an [IntegrityToken] on success, or a
      * failure if the request could not be fulfilled.
      */
-    override suspend fun request(): Result<IntegrityToken> {
+    override suspend fun request(): Result<IntegrityToken> = runCatching {
         warmUp()
 
-        val result = tokenProvider?.fold(
-            onSuccess = { it.request(requestHashProvider) },
-            onFailure = { Result.failure(it) },
-        ) ?: Result.failure(MissingTokenProvider())
+        val provider = checkNotNull(tokenProvider) {
+            "GooglePlayIntegrityClient is missing a token provider"
+        }.getOrThrow()
 
-        return result.fold(
-            onSuccess = { Result.success(it) },
-            onFailure = { throwable ->
-                if (throwable.tokenHasExpired) {
-                    refreshTokenProvider()
-                    return request()
-                }
-
-                Result.failure(throwable)
-            },
-        )
+        return provider.request(requestHashProvider).onFailure {
+            if (it.tokenHasExpired) {
+                refreshTokenProvider()
+                return request()
+            }
+        }
     }
 
     private suspend fun refreshTokenProvider() {
@@ -253,13 +247,6 @@ class GooglePlayIntegrityClient(
  */
 class InvalidProjectNumber :
     IllegalStateException("GoogleProjectNumber is Invalid.")
-
-/**
- * Thrown when a token request is attempted without an initialized
- * [TokenProvider].
- */
-class MissingTokenProvider :
-    IllegalStateException("GooglePlayIntegrityClient is missing a token provider")
 
 private val Throwable.tokenHasExpired: Boolean
     get() = (this as? StandardIntegrityException)?.tokenProviderHasExpired ?: false
